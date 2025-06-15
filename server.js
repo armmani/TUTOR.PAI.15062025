207c8363f1e0deaceb52712bb4689f6dcbc47355
armmani <arm.maniii@gmail.com>
2025-06-15 09:36:46 +0700

    First Draft

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 3100
+const app = express()
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`)
+})
